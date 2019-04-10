4ff972f44625bb0c282cc359733ab48ea1040d49
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-04-10 12:31:11 -0700

    Imported Menu.less into index.less. Created a menu and menuButton class. Toggled the menu--open class

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,38 @@
 
 const toggleMenu = () => {
+  menu.element.classList.toggle("menu--open")
+  
   // Toggle the "menu--open" class on your menu refence. 
 }
 
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+
+class Menu {
+  constructor(element) {
+    this.element = element;
+  }
+}
+
+const menuContent = document.querySelector('.menu')
+
+
+const menu = new Menu(menuContent);
+
+
 // create a reference to the ".menu-button" class
-const menuButton;
+
+
+class MenuButton {
+  constructor(menuButton) {
+    this.menuButton = menuButton;
+    this.menuButton.addEventListener('click', toggleMenu);
+  }
+}
+
+const menuButtonElement = document.querySelector('.menu-button')
+
+const menuButton = new MenuButton(menuButtonElement)
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
